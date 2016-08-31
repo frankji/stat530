5939497d6acdbe2129db2ce1ce76607794e34b9f
JiDingjue <frankji.cbb@gmail.com>
2016-08-31 13:32:42 -0400

    week1'

---
+++ week1/Hoops/FlightDelays/FlightDelays.R
@@ -0,0 +1,45 @@
+#
+# Flight Delay Exploration
+#
+# http://stat-computing.org/dataexpo/2009/the-data.html
+
+##
+## Aside: the following code was used to create a subset of the
+## data for STAT 230/530.  I include the code for completeness.
+## The original 1995 full data could be downloaded, above, but
+## are too large (about 500MB) to be a good idea for us right now!
+## So please don't try to run the following code; if you want to
+## do something, start with the United Airlines subset that I
+## provide and begin to explore.
+##
+
+x <- read.csv("1995.csv", as.is=TRUE)
+
+dim(x)                    # How big is this data set?
+names(x)                  # What are the variable names?
+table(x$UniqueCarrier)    # A one-way contingency table of the carriers
+
+# Here, I subset those rows of the data set corresponding to United
+# Airlines (UA) AND having a non-missing value in DepDelay.  If you
+# have used R before and are familiar with the subset() command, you
+# might be in the wrong class.  Consider STAT 361 next semester.
+# Please do not use subset() prior to the Fall Break.  After the Fall
+# Break, if you really like it and know what you are doing, fine.
+
+y1 <- x[x$UniqueCarrier=="UA" & !is.na(x$DepDelay),]    # Preferred
+y2 <- subset(x, UniqueCarrier=="UA" & !is.na(DepDelay)) # Okay after Fall Break
+identical(y1, y2)   # Proving the two lines above are equivalent
+
+# Strongly recommended instead of using read.csv():
+write.table(y1, "UA1995.csv", row.names=F, col.names=T, sep=",")
+
+##
+## Now starting with United Airlines
+##
+
+x <- read.csv("UA1995.csv", as.is=TRUE)
+
+dim(x)
+names(x)
+
+
---
+++ week1/Hoops/goldsheet.R
@@ -0,0 +1,72 @@
+# Basic data scrape from Goldsheet
+#
+# STAT 230/530: I'll use this during shopping period as an example
+# of something to work towards.  I hope it helps you answer the question,
+# "is this course right for me?"
+#
+# - If you're thinking, "wow, cool, I'd love to be study problems like
+#   this but I'm worried about my computing abilities!" then you are in
+#   the right place!
+# - If you're thinking something like, "I could do that in Python pretty
+#   easily, what's the big deal?" then you're in the wrong place.
+# - If you're thinking, "That might be a lot of work and I'm far more
+#   interested in the techniques, methods, and theory of data analysis,
+#   data mining, and machine learning..." then you're in the wrong place.
+#
+################################################################################
+# In case we have trouble reading from the web, I provide a
+# local copy of the page.  The web site also sometimes has slow responses.
+
+#page <- 'http://www.goldsheet.com/historic/cbblog12.html'
+#x <- scan(page, what="", sep="\n")   # SLOW!
+#length(x)
+#write(x, "cbblog12.html")
+
+# Instead, read in from local copy:
+x <- scan("cbblog12.html", what="", sep="\n")
+length(x)
+
+# First bit of cleaning:
+x <- gsub("&#160;", "", x, fixed=TRUE)
+x <- gsub("</span>", ",", x, fixed=TRUE)
+x <- gsub("<[^<>]*>", "", x)
+y <- strsplit(x, ",")
+
+# Sanity check:
+table(sapply(y, length))
+
+# More data reduction, tightening the focus:
+z <- y[sapply(y, length)==7]
+zz <- matrix(unlist(z), ncol=7, byrow=TRUE)
+
+# We're in the home stretch:
+scores <- strsplit(zz[,5], "-")
+table(sapply(scores, length))
+scores <- matrix(as.numeric(unlist(scores)), ncol=2, byrow=TRUE)
+
+ps <- gsub("'", ".5", zz[,4])
+ps <- gsub("P", 0, ps)
+
+x <- data.frame(pointspread=as.numeric(ps),
+                score1=scores[,1], score2=scores[,2],
+                scorediff=scores[,2]-scores[,1],
+                loc=zz[,6],
+                stringsAsFactors=FALSE)
+
+plot(x$pointspread, x$scorediff,
+     xlab="Pre-Game Point Spread",
+     ylab="Actual Game Score Differential")
+abline(0, 1)
+
+lm.result <- lm(scorediff ~ pointspread, data=x)
+abline(lm.result, col="red", lwd=3)
+summary(lm.result)
+
+#
+# Other very very useful functions for data cleaning and manipulation:
+#
+# - regexpr()
+# - substring()
+# - nchar()
+# - grep()
+# ...
